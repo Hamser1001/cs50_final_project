8b335e14838841ff2a9ba58c0fc792000364d36e
Hamser1001 <upscale.mind50@gmail.com>
2026-04-07 16:56:28 +0100

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request
 from flask_session import Session
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -17,3 +18,12 @@ def index():
         password = request.form.get("password")
         print(f"username: {username}, password: {password}")
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(f"username: {username}, password: {password}")
+    return render_template("register.html")
