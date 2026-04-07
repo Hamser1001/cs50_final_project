fc3f5bb7d75e4c40bc80eac23392961db77a1b9b
Hamser1001 <upscale.mind50@gmail.com>
2026-04-07 16:04:00 +0100

    update

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, session, request
+from flask_session import Session
+
+app = Flask(__name__)
+
+# Configure the session / use filesystem
+app.config["SESSION_PERMANANT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    # username = request.form.get("username")
+    # password = request.form.get("password")
+    # print(f"username: {username}, password: {password}")
+    return render_template("index.html")
